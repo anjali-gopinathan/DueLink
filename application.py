b1f59cd195fe2e3acaf1189dc6d4f889105a43c1
Akhilesh Bellathur <abellath@usc.edu>
2019-10-26 23:20:02 -0700

    eowiefj

--- application.py
+++ application.py
@@ -16,9 +16,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return "Hello world. about page"
-
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
