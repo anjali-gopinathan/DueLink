ef3d92388d9d72b74ebda4d18054038edd419f41
Fireborn <cfeuerbo@usc.edu>
2019-10-26 19:52:53 -0700

    Updated CSS to make it look prettier
    Now it works!

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html","index.css")
 
 @app.route('/about')
 def about():
